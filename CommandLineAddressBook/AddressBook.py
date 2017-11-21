bf5e8ea0c4e12826e123dc2100963d68e1743518
lixibo <alanlxb.xmu@gmail.com>
2017-11-21 11:36:14 +0800

    Finish CommandLineAddressBook program.

--- CommandLineAddressBook/AddressBook.py
+++ CommandLineAddressBook/AddressBook.py
@@ -14,17 +14,47 @@ def deletePerson(name):
     PersonCommon.removePerson(name)
 
 
-def editPerson(name):
+def editPerson(name, identity, email, phone):
     """修改人员"""
-    # TODO
-    pass
+    person = Person(name, identity, email, phone)
+    PersonCommon.savePerson(person)
 
 
 def getPerson(name):
     """查找人员"""
-    return PersonCommon.readPerson(name)
+    person = PersonCommon.readPerson(name)
+    if person:
+        print("{}'s information is as follow:".format(name))
+        print("name:{},identity:{},email:{},phone:{}".format(person.name, person.identity, person.email, person.phone))
+    else:
+        print("Can not find person: {}!".format(name))
 
 
-addPerson("lixibo", "family", "2324234234@gmail.com", "18050056054")
-print(getPerson("lixibo"))
-deletePerson("lixibo")
+def browseAll():
+    addreddbook = PersonCommon.readAllPerson()
+    for name, person in addreddbook.items():
+        print("name:{},identity:{},email:{},phone:{}".format(person.name, person.identity, person.email, person.phone))
+
+
+# Test Code
+addPerson("alan1", "family", "111111111@gmail.com", "180111111111")
+addPerson("alan2", "friend", "222222222@gmail.com", "180222222222")
+addPerson("alan3", "friend", "333333333@gmail.com", "180333333333")
+print("====print all people==========================================")
+browseAll()
+print("====test find someone========================================")
+getPerson("alan1")
+print()
+getPerson("alan11111")
+print("====test edit=================================================")
+print("====before edit alan3")
+getPerson("alan3")
+print("====after edit alan3")
+editPerson("alan3", "family", "aaaaaaaaa@gmail.com", "18099999999")
+getPerson("alan3")
+print("====test delete===============================================")
+print("====before delete alan2")
+getPerson("alan2")
+deletePerson("alan2")
+print("====after delete alan2")
+getPerson("alan2")
--- CommandLineAddressBook/PersonCommon.py
+++ CommandLineAddressBook/PersonCommon.py
@@ -2,25 +2,60 @@
 import pickle
 import os
 
-filePath = "personDictionary/"
+fileName = "AddressBookDictionary"
 
 
 def savePerson(person):
     """持久化保存人员"""
-    filename = filePath + person.name
-    file = open(filename, "wb")  # 以“二进制写”模式打开文件
-    pickle.dump(person, file)  # 转储对象至文件
+    # 获取人员列表
+    addressbook = {}
+    if os.path.isfile(fileName):
+        file = open(fileName, "rb")  # 以“二进制读”模式打开文件
+        addressbook = pickle.load(file)
+        file.close()
+
+    # 给列表增加人员，覆盖同名人员
+    addressbook[person.name] = person
+
+    # 持久化人员列表
+    file = open(fileName, "wb")  # 以“二进制写”模式打开文件
+    pickle.dump(addressbook, file)  # 转储对象至文件
     file.close()
 
 
 def readPerson(name):
     """从文件中读取电话簿并返回相应的人员"""
-    filename = filePath + name
-    file = open(filename, "rb")  # 以“二进制读”模式打开文件
+    if not os.path.isfile(fileName):
+        return None
+
+    file = open(fileName, "rb")  # 以“二进制读”模式打开文件
+    addressbook = pickle.load(file)
+    return addressbook[name] if name in addressbook else None
+
+
+def readAllPerson():
+    """查找所有人员"""
+    if not os.path.isfile(fileName):
+        return None
+    file = open(fileName, "rb")  # 以“二进制读”模式打开文件
     return pickle.load(file)
 
 
 def removePerson(name):
     """移除电话簿对应人员"""
-    filename = filePath + name
-    os.remove(filename)
+    # 获取人员列表
+    addressbook = {}
+    if os.path.isfile(fileName):
+        file = open(fileName, "rb")  # 以“二进制读”模式打开文件
+        addressbook = pickle.load(file)
+        file.close()
+
+    # 删除对应人员
+    if name not in addressbook:
+        return
+    del addressbook[name]
+
+    # 持久化人员列表
+    file = open(fileName, "wb")  # 以“二进制写”模式打开文件
+    pickle.dump(addressbook, file)  # 转储对象至文件
+    file.close()
